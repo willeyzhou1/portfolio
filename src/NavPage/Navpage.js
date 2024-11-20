21d85ae3a655b9c37c02bee58a82a80cb9cb6378
willersss <willzhou000@gmail.com>
2024-11-19 17:32:30 -0800

    NavPage Created

--- src/NavPage/Navpage.js
+++ src/NavPage/Navpage.js
@@ -1,3 +1,21 @@
 import React, { Component } from 'react'
 import { Link, useLocation } from "react-router-dom";
+import './Navpage.css';
 
+export default function Navpage() {
+    return (
+        <nav className="NavBar">
+            <ul className="elements">
+                <li>
+                    <a href='/about'>About Me!</a>
+                </li>
+                <li>
+                    <a href='/projects'>My Projects</a>
+                </li>
+                <li>
+                    <a href='/contact'>Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
---
+++ src/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link, useLocation } from "react-router-dom";
+import './Projects.css';
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>Projects</div>
+    )
+  }
+}
