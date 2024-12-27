839cc9765f9e7c1d8108c125ed67a1e45e733c99
willersss <willzhou000@gmail.com>
2024-12-26 16:19:52 -0800

    Contact page made

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import './Contact.css'
 
 export default class Contact extends Component {
   render() {
     return (
-      <div id='contact'>contact</div>
+      <div className='contact' id='contact'>
+        <h1>Contact Me!</h1>
+        <textarea placeholder='please write something in here'></textarea>
+      </div>
     );
   }
 }
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,13 +4,23 @@ import ystemandchess from '../assets/acm.png'
 import ProjectComponent from '../Components/projectComponent.js'
 
 export default class Projects extends Component {
+  
   render() {
     return (
       <div className='pages' id='projects'>
         <h1>Projects</h1>
         <p>These are some of the projects that I have done :D</p>
-        <li className='projects'>
-          <ul>
+        <div className='projects'>
+          <div className='project'>
+            <a className='element' href='https://github.com/acmucsd-projects/sp23-hack-team-3'>
+            <img src={ystemandchess} height='200px' width='200px'></img>
+              <div className='text'>
+                <h2>Eventify</h2>
+                <p>Created a website that allows for 1000+ students to advertise events happening on campus. Worked as a frontend developer.</p>
+              </div>
+            </a>
+          </div>
+          <div className='project'>
             <a className='element' href='https://github.com/acmucsd-projects/sp23-hack-team-3'>
               <img src={ystemandchess} height='200px' width='200px'></img>
                 <div className='text'>
@@ -18,8 +28,8 @@ export default class Projects extends Component {
                   <p>Created a website that allows for 1000+ students to advertise events happening on campus. Worked as a frontend developer.</p>
                 </div>
             </a>
-          </ul>
-          <ul>
+          </div>
+          <div className='project'>
             <a className='element' href='https://github.com/acmucsd-projects/sp23-hack-team-3'>
               <img src={ystemandchess} height='200px' width='200px'></img>
                 <div className='text'>
@@ -27,17 +37,8 @@ export default class Projects extends Component {
                   <p>Created a website that allows for 1000+ students to advertise events happening on campus. Worked as a frontend developer.</p>
                 </div>
             </a>
-          </ul>
-          <ul>
-            <a className='element' href='https://github.com/acmucsd-projects/sp23-hack-team-3'>
-              <img src={ystemandchess} height='200px' width='200px'></img>
-                <div className='text'>
-                  <h2>Eventify</h2>
-                  <p>Created a website that allows for 1000+ students to advertise events happening on campus. Worked as a frontend developer.</p>
-                </div>
-            </a>
-          </ul>
-        </li>
+          </div>
+        </div>
 
       </div>
     )
